d09a697ac57b3c13078d07901f8f16c6d36bff21
Aldo Monteiro <aldomonteiro@gmail.com>
2018-11-19 10:29:29 -0200

    removed publish_pages

--- logs/db.js
+++ logs/db.js
@@ -2,34 +2,25 @@ var pagesArray = new Array();
 pagesArray.push('947115235484171');
 pagesArray.push('307519123184673');
 
-var userID = '171581173796245';
+var userID = '174457213508641';
 
-print('* Connected to the database');
+printjson('* Connected to the database');
 
 // delete pricings, sizes and flavors for the page:
 for (var i = 0; i < pagesArray.length; i++) {
     var pageID = pagesArray[i];
-    print('*** PageID:' + pageID + '***');
+    ('*** PageID:' + pageID + '***');
 
-    print('* Deleting pricings:');
-    db.pricings.deleteMany({ pageId: pageID });
-    print('* Deleting sizes:');
-    db.sizes.deleteMany({ pageId: pageID });
-    print('* Deleting flavors:');
-    db.flavors.deleteMany({ pageId: pageID });
-    print('* Deleting stores:');
-    db.stores.deleteOne({ pageId: pageID });
-    print('* Deleting items:');
-    db.items.deleteMany({ pageId: pageID });
-    print('* Deleting orders:');
-    db.orders.deleteMany({ pageId: pageID });
-    print('* Deleting customers:');
-    db.customers.deleteMany({ pageId: pageID });
-    print('* Deleting page:');
-    db.pages.deleteOne({ id: pageID });
+    printjson('* Deleting pricings:', db.pricings.deleteMany({ pageId: pageID }));
+    printjson('* Deleting sizes:', db.sizes.deleteMany({ pageId: pageID }));
+    printjson('* Deleting flavors:', db.flavors.deleteMany({ pageId: pageID }));
+    printjson('* Deleting stores:', db.stores.deleteOne({ pageId: pageID }));
+    printjson('* Deleting items:', db.items.deleteMany({ pageId: pageID }));
+    printjson('* Deleting orders:', db.orders.deleteMany({ pageId: pageID }));
+    printjson('* Deleting customers:', db.customers.deleteMany({ pageId: pageID }));
+    printjson('* Deleting page:', db.pages.deleteOne({ id: pageID }));
 }
 
-print('* Deleting user:');
-db.users.deleteOne({ userID: userID });
+printjson('* Deleting user:', db.users.deleteOne({ userID: userID }));
 
 
